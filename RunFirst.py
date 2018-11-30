7b37ae6d3898e708112b4f563c1f8287a5e6d5ba
skulldriver101 <44977317+skulldriver101@users.noreply.github.com>
2018-11-30 10:11:29 -0800

    Update RunFirst.py

--- RunFirst.py
+++ RunFirst.py
@@ -1,7 +1,9 @@
 sudo apt-get update
 sudo apt-get install build-essential python-dev
+sudo apt-get install git-core
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git
 cd Adafruit_Python_DHT
+sudo apt-get install build-essential python-dev
 sudo python setup.py install
 sudo python3 setup.py install
 cd ~
