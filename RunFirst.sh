2d5433fe45e42b37503d3203f3eebf28c5bff267
skulldriver101 <44977317+skulldriver101@users.noreply.github.com>
2018-12-11 13:46:58 -0600

    Update RunFirst.sh

--- RunFirst.sh
+++ RunFirst.sh
@@ -2,6 +2,8 @@ cd ..
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install build-essential python-dev
+#needs install
+git clone https://github.com/adafruit/io-client-python
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git
 cd Adafruit_Python_DHT
 sudo apt-get install build-essential python-dev
