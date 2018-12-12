35152f079f633f02bc9dea804f7d87af6dc8233c
skulldriver101 <44977317+skulldriver101@users.noreply.github.com>
2018-12-12 13:41:48 -0600

    Update Adafruitio.py

--- Adafruitio.py
+++ Adafruitio.py
@@ -28,29 +28,29 @@ from Adafruit_IO import Client, Feed
 DHT_READ_TIMEOUT = 5
 
 # Pin connected to DHT22 data pin
-DHT_DATA_PIN = 26
+DHT_DATA_PIN = 20
 
 # Set to your Adafruit IO key.
 # Remember, your key is a secret,
 # so make sure not to publish it when you publish this code!
-ADAFRUIT_IO_KEY = 'YOUR_AIO_KEY'
+ADAFRUIT_IO_KEY = 'c907dc9fee4f4a978b830c2e126345b6'
 
 # Set to your Adafruit IO username.
 # (go to https://accounts.adafruit.com to find your username).
-ADAFRUIT_IO_USERNAME = 'YOUR_AIO_USERNAME'
+ADAFRUIT_IO_USERNAME = 'Mschroer'
 
 # Create an instance of the REST client.
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
 # Set up Adafruit IO Feeds.
-temperature_feed = aio.feeds('temperature')
-humidity_feed = aio.feeds('humidity')
+temperature_feed = aio.feeds('Temp')
+humidity_feed = aio.feeds('Humidity')
 
-# Set up DHT22 Sensor.
-dht22_sensor = Adafruit_DHT.DHT22
+# Set up DHT11 Sensor.
+dht11_sensor = Adafruit_DHT.DHT11
 
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(dht22_sensor, DHT_DATA_PIN)
+    humidity, temperature = Adafruit_DHT.read_retry(dht11_sensor, DHT_DATA_PIN)
     if humidity is not None and temperature is not None:
         print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity))
         # Send humidity and temperature feeds to Adafruit IO
@@ -59,7 +59,7 @@ while True:
         aio.send(temperature_feed.key, str(temperature))
         aio.send(humidity_feed.key, str(humidity))
     else:
-        print('Failed to get DHT22 Reading, trying again in ', DHT_READ_TIMEOUT, 'seconds')
+        print('Failed to get DHT11 Reading, trying again in ', DHT_READ_TIMEOUT, 'seconds')
     # Timeout to avoid flooding Adafruit IO
     time.sleep(DHT_READ_TIMEOUT)
 
